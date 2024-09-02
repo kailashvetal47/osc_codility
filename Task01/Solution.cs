792c26925bafa8a77388be0f90d63c8a4cb499fe
Kailash Vetal <kailash.vetal47@gmail.com>
2024-09-03 00:54:21 +0530

    Refactor- Format code.

--- Task01/Solution.cs
+++ Task01/Solution.cs
@@ -2,20 +2,20 @@ namespace code_challenges.Task01;
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Drawing;
 
 
-public class Point2D {
-  public int x;
-  public int y;
+public class Point2D
+{
+    public int x;
+    public int y;
 
-  public Point2D(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+    public Point2D(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
 
-   public override bool Equals(object obj)
+    public override bool Equals(object obj)
     {
         if (obj == null || GetType() != obj.GetType())
         {
@@ -28,67 +28,47 @@ public class Point2D {
 
     public override int GetHashCode()
     {
-        return HashCode.Combine(x, y); 
+        return HashCode.Combine(x, y);
     }
 };
 
-public class Solution {
+public class Solution
+{
+    readonly Point2D origin = new Point2D(0, 0);
 
-   readonly Point2D ORIGIN = new Point2D(0,0) {};
-
-    public int solution(Point2D[] A) 
+    public int solution(Point2D[] points)
     {
+        int distinctLines = 0;
+        HashSet<Point2D> processedPoints = new HashSet<Point2D>();
 
-      int result = 0;
-      HashSet<Point2D> processedPoints = new HashSet<Point2D>();
-
-      foreach( Point2D p in  A )
-      {
-        Console.WriteLine("Point : " + p.x + " , " + p.y);
-        if(p.Equals(ORIGIN))
+        foreach (Point2D point1 in points)
         {
-          continue;
-        }
+            if (point1.Equals(origin))
+            {
+                continue;
+            }
 
-        if(!processedPoints.Contains(p))
-        {
-          Console.WriteLine("Processing Line Origin to P " + p.x + " , " + p.y);
+            if (!processedPoints.Contains(point1))
+            {
+                distinctLines++;
+                processedPoints.Add(point1);
 
-          result++;
-          processedPoints.Add(p);
-          foreach( Point2D q in  A )
-          {
-            //hint: All origin(0,0) will be added in the processedPoints automatically as
-            //      they form line with any point and origin.
-             if(!processedPoints.Contains(q) )
-             {
-              Console.WriteLine("Processing Line P ( " + p.x + " , " + p.y + ")" + " to Q (" + q.x + " , " + q.y + ")");
-                if(IsPointOnLine(ORIGIN,p,q))
+                foreach (Point2D point2 in points)
                 {
-                  processedPoints.Add(q);
+                    if (!processedPoints.Contains(point2) && IsPointOnLine(origin, point1, point2))
+                    {
+                        processedPoints.Add(point2);
+                    }
                 }
-             }
-
-          }
-         
-
-      
+            }
         }
-      }
 
-      return result;
+        return distinctLines;
     }
 
-    private bool IsPointOnLine(Point2D p1, Point2D p2, Point2D p)
+    private bool IsPointOnLine(Point2D point1, Point2D point2, Point2D point3)
     {
-      bool result = false;
-      var area = (p2.x - p1.x) * (p.y - p1.y) - (p.x - p1.x) * (p2.y - p1.y);
-      if(Math.Abs(area) <= 1e-6)
-      {
-        result = true;
-      }
-      Console.WriteLine("Result : " + result );
-      return result;
-
+        var area = (point2.x - point1.x) * (point3.y - point1.y) - (point3.x - point1.x) * (point2.y - point1.y);
+        return Math.Abs(area) <= 1e-6;
     }
 }
