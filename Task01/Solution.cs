0c201aa54d5ffcd085b02d15748aaf9a3b12b295
Kailash Vetal <kailash.vetal47@gmail.com>
2024-09-03 00:58:16 +0530

    Task 01 complete (#1)
    * Adds Test data for solution-1
    
    * Update Task01 tests
    
    * Created valid test data
    
    * Fixed Origin Test
    
    * Fixed Test - GivenThreeLinearPoints_ExpectResultOne
    
    * Added new test - GivenThreeLinearPoints_ExpectResultOne
    
    * Refactor - Removes old code
    
    * Refacor - Adds Equals method for Point2D
    
    * Tests - Add more tests using AI
    
    * Refactor- Format code.

--- Task01/Solution.cs
+++ Task01/Solution.cs
@@ -2,47 +2,73 @@ namespace code_challenges.Task01;
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-// you can also use other imports, for example:
-// using System.Collections.Generic;
 
-// you can write to stdout for debugging purposes, e.g.
-// Console.WriteLine("this is a debug message");
 
-class Point2D {
-  public int x;
-  public int y;
-};
+public class Point2D
+{
+    public int x;
+    public int y;
 
-class Solution {
-    public int solution(Point2D[] A) {
-
-      int result = 0;
-      var origin = new Point2D {x = 0, y = 0};
-        foreach( var p in A)
-        {
-            int rayCount = 0;
-
-            foreach(var q in A)
-            {
-                if(IsPointOnLine(origin, p, q))
-                {
-                    rayCount++;
-                }
-            }
-
-        }
-
-      return result;
+    public Point2D(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
     }
 
-    private bool IsPointOnLine(Point2D p1, Point2D p2, Point2D p)
+    public override bool Equals(object obj)
     {
-            // Calculate the cross product of vectors p1-p and p2-p
-                int crossProduct = (p2.X - p1.X) * (p.Y - p1.Y) - (p.X - p1.X) * (p2.Y - p1.Y);
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
 
-                // A point is on the line if the cross product is zero
-                return crossProduct == 0;
+        Point2D other = (Point2D)obj;
+        return (x == other.x) && (y == other.y);
+    }
 
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, y);
+    }
+};
+
+public class Solution
+{
+    readonly Point2D origin = new Point2D(0, 0);
+
+    public int solution(Point2D[] points)
+    {
+        int distinctLines = 0;
+        HashSet<Point2D> processedPoints = new HashSet<Point2D>();
+
+        foreach (Point2D point1 in points)
+        {
+            if (point1.Equals(origin))
+            {
+                continue;
+            }
+
+            if (!processedPoints.Contains(point1))
+            {
+                distinctLines++;
+                processedPoints.Add(point1);
+
+                foreach (Point2D point2 in points)
+                {
+                    if (!processedPoints.Contains(point2) && IsPointOnLine(origin, point1, point2))
+                    {
+                        processedPoints.Add(point2);
+                    }
+                }
+            }
+        }
+
+        return distinctLines;
+    }
+
+    private bool IsPointOnLine(Point2D point1, Point2D point2, Point2D point3)
+    {
+        var area = (point2.x - point1.x) * (point3.y - point1.y) - (point3.x - point1.x) * (point2.y - point1.y);
+        return Math.Abs(area) <= 1e-6;
     }
 }
--- Task01/SolutionTests.cs
+++ Task01/SolutionTests.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using NUnit.Framework;
 
 namespace code_challenges.Task01;
@@ -9,21 +13,95 @@ public class SolutionTests
 
      private Solution _task;
 
+
     [SetUp]
     public void Setup()
     {
         _task = new Solution();
     }
 
-    [TestCase(new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 0, TestName = "SimpleTest")]
-    [TestCase(new int[] { }, ExpectedResult = 0, TestName = "EmptyArray")]
-    [TestCase(new int[] { 42 }, ExpectedResult = 0, TestName = "SingleElementArray")]
 
-
-    public int GivenValidInput_ShouldGiveExpectedResult(int[] input)
+    [Test(ExpectedResult = 0, Description = "GivenOriginAsInput_ExpectResultZero")]
+    public int GivenOriginAsInput_ExpectResultZero ()
     {
+        Point2D[] input = {new Point2D(0,0)};
         int result = _task.solution(input);
         return result;
     }
+
+    [Test(ExpectedResult = 3, Description = "01-GivenThreeNonLinearPoints_ExpectResultThree")]
+    public int GivenThreeNonLinearPoints_ExpectResultThree ()
+    {
+        Point2D[] input = [new Point2D(0,1), new Point2D(1,0), new Point2D(1,1)];
+        int result = _task.solution(input);
+        return result;
+    }
+
+    [Test(ExpectedResult = 1, Description = "GivenThreeLinearPoints_ExpectResultOne")]
+    public int GivenThreeLinearPoints_ExpectResultOne ()
+    {
+        Point2D[] input = [new Point2D(1,1), new Point2D(2,2), new Point2D(3,3)];
+        int result = _task.solution(input);
+        return result;
+    }
+
+    [Test(ExpectedResult = 3, Description = "GivenThreeLinearPoints_ExpectResultOne")]
+    public int GivenOriginAsSecondPointFourNonLinearPoints_ExpectResultThree ()
+    {
+        Point2D[] input = [new Point2D(0,1), new Point2D(0,0),  new Point2D(1,0), new Point2D(1,1)];
+        int result = _task.solution(input);
+        return result;
+    }
+
+     [Test(ExpectedResult = 3, Description = "GivenThreeLinearPoints_ExpectResultOne")]
+    public int GivenBoundryConditionPoints_InThreeNonLinearPoints_ExpectResultThree ()
+    {
+        Point2D[] input = [new Point2D(1000000,1), new Point2D(0,0),  new Point2D(1,0), new Point2D(1,1)];
+        int result = _task.solution(input);
+        return result;
+    }
+
+    // AI-Generated Tests
+
+    [Test(ExpectedResult = 1, Description = "GivenSingleNonOriginPoint_ExpectResultOne")]
+    public int GivenSingleNonOriginPoint_ExpectResultOne()
+    {
+        Point2D[] input = [new Point2D(5, 10)];
+        int result = _task.solution(input);
+        return result;
+    }
+
+    [Test(ExpectedResult = 2, Description = "GivenTwoPointsWithSameXDifferentY_ExpectResultTwo")]
+    public int GivenTwoPointsWithSameXDifferentY_ExpectResultTwo()
+    {
+        Point2D[] input = [new Point2D(2, 3), new Point2D(2, -5)];
+        int result = _task.solution(input);
+        return result;
+    }
+
+    [Test(ExpectedResult = 2, Description = "GivenTwoPointsWithSameYDifferentX_ExpectResultTwo")]
+    public int GivenTwoPointsWithSameYDifferentX_ExpectResultTwo()
+    {
+        Point2D[] input = [new Point2D(-4, 7), new Point2D(8, 7)];
+        int result = _task.solution(input);
+        return result;
+    }
+
+    [Test(ExpectedResult = 1, Description = "GivenDuplicatePoints_ExpectResultOne")]
+    public int GivenDuplicatePoints_ExpectResultOne()
+    {
+        Point2D[] input = [new Point2D(1, 2), new Point2D(1, 2), new Point2D(1, 2)];
+        int result = _task.solution(input);
+        return result;
+    }
+
+    [Test(ExpectedResult = 2, Description = "GivenMixOfLinearAndNonLinearPoints_ExpectResultTwo")]
+    public int GivenMixOfLinearAndNonLinearPoints_ExpectResultTwo()
+    {
+        Point2D[] input = [new Point2D(1, 1), new Point2D(2, 2), new Point2D(3, 3), new Point2D(1, 2)];
+        int result = _task.solution(input);
+        return result;
+    }
+
 
 }
